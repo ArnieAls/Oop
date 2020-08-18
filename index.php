c8bd475217e5ce118204f800e859580421b9e1c4
ArnieAls <balciunasarnoldas@gmail.com>
2020-08-18 14:16:18 +0300

    bomba

--- index.php
+++ index.php
@@ -1,25 +1,42 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>bomba</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>OOP PHP</title>
     <style>
-        img {
-            width: 0.2 * <?php print date('s'); ?>'px'
+        body {
+            margin: 0;
+            padding: 0;
         }
-        .bg {
-            width: 400px;
-            height: 500px;
-            background: url("https://img.freepik.com/free-vector/cartoon-bomb-pop-art-style_81894-2844.jpg?size=338&ext=jpg");
-            background-repeat: no-repeat;
+        .bomb-box {
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+        .bomb {
+            background-image: url("https://i.imgur.com/1q8YiXb.png");
+            background-size: cover;
+            width: <?php print 10 * date('s'); ?>px;
+            height: <?php print 10 * date('s'); ?>px;
+        }
+        span {
+            text-align: center;
+            color: aqua;
+            font-size: 25px;
+            font-weight: bold;
+            border: 1px solid red;
         }
     </style>
 </head>
 <body>
-<?php
-print date('s');
-?>
-<div class="bg"></div>
+<div class="bomb-box">
+    <div class="bomb"></div>
+    <span><?php print date('s'); ?></span>
+</div>
 </body>
 </html>
