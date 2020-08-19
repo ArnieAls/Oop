8b1c5590f86b46de692511623648d7f0694aa2ba
ArnieAls <balciunasarnoldas@gmail.com>
2020-08-19 11:50:43 +0300

    keliones skaiciuokle

--- index.php
+++ index.php
@@ -5,40 +5,21 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>OOP PHP</title>
-    <style>
-        body {
-            margin: 0;
-        }
-        .bomb-block {
-            min-height: 100vh;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-        }
-        .bomb {
-            background-image: url("https://img.freepik.com/free-vector/cartoon-bomb-pop-art-style_81894-2844.jpg?size=338&ext=jpg");
-            background-size: cover;
-            width: <?php print 10 * date('s'); ?>px;
-            height: <?php print 10 * date('s'); ?>px;
-        }
-        span {
-            text-align: center;
-            color: red;
-            font-size: 16px;
-        }
-        .explosion-00 {
-            background-image: url("https://upload.wikimedia.org/wikipedia/commons/7/79/Operation_Upshot-Knothole_-_Badger_001.jpg");
-            background-size: cover;
-        }
-    </style>
-</head>
+    <title>fuel consumption</title>
 <body>
-<div class="bomb-block">
-    <div class="bomb"></div>
-    <span><?php print date('s'); ?></span>
-    <div class="bomb explosion-<?php print date('s'); ?>></div>
-</div>
+<?php
+
+$distance = rand(0, 1000);
+$consumption = 7.5/100;
+$price_l = 1.2;
+$fuel_total = $distance * $consumption;
+$price_trip = $fuel_total * $price_l;
+?>
+<h1>Keliones skaiciuokle</h1>
+<ul>
+    <li>Nuvaziuota distancija <?php print $distance; ?></li>
+    <li>Kaina <?php print $price_trip; ?> pinigu</li>
+    <li>Sunaudota <?php print $fuel_total; ?></li>
+</ul>
 </body>
 </html>
