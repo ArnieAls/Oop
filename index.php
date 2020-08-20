ca4db4563f87e835b17d73125ef70f5f63bbf439
ArnieAls <balciunasarnoldas@gmail.com>
2020-08-20 10:22:20 +0300

    creating array

--- index.php
+++ index.php
@@ -5,21 +5,29 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>fuel consumption</title>
+    <title>array</title>
+</head>
 <body>
 <?php
 
-$distance = rand(0, 1000);
-$consumption = 7.5/100;
-$price_l = 1.2;
-$fuel_total = $distance * $consumption;
-$price_trip = $fuel_total * $price_l;
+$produktai = [
+    [
+        'pavadinimas' => 'Grietinė',
+        'dydis' => 'big',
+        'kiekis' => 1
+    ],
+    [
+        'pavadinimas' => 'Baklažanas',
+        'dydis' => 'small',
+        'kiekis' => 1
+    ],
+    [
+        'pavadinimas' => 'Kiaušinis',
+        'dydis' => 'big',
+        'kiekis' => 2
+    ],
+];
+var_dump($produktai);
 ?>
-<h1>Keliones skaiciuokle</h1>
-<ul>
-    <li>Nuvaziuota distancija <?php print $distance; ?></li>
-    <li>Kaina <?php print $price_trip; ?> pinigu</li>
-    <li>Sunaudota <?php print $fuel_total; ?></li>
-</ul>
 </body>
 </html>
