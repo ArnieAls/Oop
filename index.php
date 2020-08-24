0b9f688817d90079f653c849becb7cdbceff6b71
ArnieAls <balciunasarnoldas@gmail.com>
2020-08-24 19:40:29 +0300

    foreach pratimai

--- index.php
+++ index.php
@@ -1,34 +1,22 @@
-<?php
-$money = rand(0, 25);
-$bokal_cost = 3.5;
-$bokal_count = floor($money / $bokal_cost);
-$money_spent = number_format($bokal_count * $bokal_cost, 2);
-$p = "Viso sumokėta $money_spent eur"
-?>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Alus</title>
-    <style>
-        .beer-container {
-            display: flex;
-        }
-        .beer {
-            background-image: url("https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1.bp.blogspot.com%2F-Ht7b9u8y7z8%2FUBkvmVbhKMI%2FAAAAAAAAF8w%2Frw_RXFF83VA%2Fs1600%2FBeer_Wallpaper%2B(62).jpg&f=1&nofb=1");
-            background-position: center;
-            background-size: cover;
-            width: 100px;
-            height: 100px;
-        }
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>2 konsultacija</title>
+  <link rel='stylesheet' type='text/css' href='css/main.php' />
 </head>
 <body>
-<div class="beer-container">
-    <?php for ($i = 1; $i <= $bokal_count; $i++): ?>
-        <div class="beer"></div>
-    <?php endfor; ?>
-</div>
-<p><?php print $p; ?></p>
+  <div class="container">
+    <?php 
+      // include 'tasks/bombTask.php';
+      // include 'tasks/btnTask.php';
+      // include 'tasks/forCycleTask.php';
+      // include 'tasks/arrays.php';
+      include 'tasks/foreach.php';
+    ?>
+    
+  </div>
+  
 </body>
 </html>
