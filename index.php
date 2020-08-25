351cb33c446accd763047b55c477044006829ddf
ArnieAls <balciunasarnoldas@gmail.com>
2020-08-25 08:29:40 +0300

    GTA hud incomplete

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>2 konsultacija</title>
-  <link rel='stylesheet' type='text/css' href='css/main.php' />
+    <meta charset="UTF-8">
+    <title>GTA part 2</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <div class="container">
-    <?php 
-      // include 'tasks/bombTask.php';
-      // include 'tasks/btnTask.php';
-      // include 'tasks/forCycleTask.php';
-      // include 'tasks/arrays.php';
-      include 'tasks/foreach.php';
-    ?>
-    
-  </div>
-  
+<div class="box-bordered">
+    <div class="hud-tablo">
+        <div class="hud-item">
+            <div class="hud-weapon fist"></div>
+        </div>
+        <div class="hud-item">
+            <div class="hud-tablo-col">
+                <div class="hud-time">19:20</div>
+                <div class="hud-item hud-progress-armor">
+                    <div class="hud-armor" style="width: 2%"></div>
+                </div>
+                <div class="hud-item hud-ammo">40</div>
+            </div>
+        </div>
+    </div>
+    <div class="hud-item hud-progress-health">
+        <div class="hud-health" style="width: 5%"></div>
+    </div>
+    <div class="hud-item hud-money">$00001000</div>
+    <div class="hud-item hud-wanted">☆☆☆☆★★</div>
+</div>
 </body>
 </html>
