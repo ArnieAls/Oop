896ddb347ec63b021627da1add743bf1f7de2865
ArnieAls <balciunasarnoldas@gmail.com>
2020-08-26 22:28:46 +0300

    slider input big eyes

--- index.php
+++ index.php
@@ -1,38 +1,25 @@
 <?php
-/**
- * Squares the number
- * @param int $x number
- * @return int squared number
- */
-function square(int $x) : int
-{
-    return $x ** 2;
-}
 var_dump($_POST);
 
-if (isset($_POST['nr']) && is_numeric($_POST['nr'])) {
-    $atsakymas = 'Atsakymas: ' . square($_POST['nr']);
-} else {
-    $atsakymas = 'Įrašykite skaičių';
-}
+$skaicius = intval($_POST['slider'] ?? 25);
+$message = empty($_POST) ? 'Paspausk mygtuką' : $skaicius;
 
-// $atsakymas = isset($_POST['nr']) ? square($_POST['nr']) : 'Įrašykite skaičių';
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
-​
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-​
 <body>
 <form method="POST">
-    Ką pakelti kvadratu: <input name="nr" type="number">
-    <button name="submit">Submit</button>
+    Dydis: <input name="slider" type="range" min="0" max="50">
+    <button name="submit">Let's do it</button>
 </form>
-<h2><?= $atsakymas; ?></h2>
+<p><?= $message; ?></p>
+<img style="width:<?= $skaicius; ?>em;" src="https://i.ebayimg.com/images/g/lXMAAOSwI4teQQEO/s-l300.jpg">
 </body>
 ​
 </html>
