33d04a051f9c761b3c5e41e3a23dec8156277b2d
ArnieAls <balciunasarnoldas@gmail.com>
2020-08-31 10:36:38 +0300

    Forma veikianti

---
+++ core/templates/form.tpl.php
@@ -0,0 +1,35 @@
+<form <?php print html_attr($form['attr']); ?>>
+    <!-- Generating fields -->
+    <?php foreach ($form['fields'] ?? [] as $field_id => $field) : ?>
+        <!-- Label -->
+        <?php if (isset($field['label'])) : ?>
+            <label>
+            <span><?php print $field['label']; ?></span>
+        <?php endif; ?>
+        <!-- Select field -->
+        <?php if ($field['type'] === 'select') : ?>
+            <select <?php print select_attr($field_id, $field); ?>>
+                <?php foreach ($field['options'] ?? [] as $option_id => $option_title) : ?>
+                    <option <?php print option_attr($option_id, $field); ?>>
+                        <?php print $option_title; ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+
+        <?php else : ?>
+            <!-- Input field -->
+            <input <?php print input_attr($field_id, $field); ?> />
+
+        <?php endif; ?>
+        <?php if (isset($field['label'])) : ?>
+            </label>
+        <?php endif; ?>
+    <?php endforeach; ?>
+
+    <!-- Generating buttons -->
+    <?php foreach ($form['buttons'] ?? [] as $button_id => $button) : ?>
+        <button <?php print button_attr($button_id, $button); ?>>
+            <?php print $button['title']; ?>
+        </button>
+    <?php endforeach; ?>
+</form>
--- index.php
+++ index.php
@@ -1,48 +1,109 @@
 <?php
-require 'functions/html.php';
+require 'core/functions/html.php';
+
 $form = [
     'attr' =>
         [
             'action' => 'index.php',
             'method' => 'POST',
             'class' => 'my-form',
-            'id' => 'login-form'
+            'id' => 'login-form',
         ],
-    'fields' => [
-        'email' => [
-            'label' => 'E-mail',
-            'type' => 'email',
-            'value' => 'test-email',
-            'extra' => [
-                'attr' => [
-                    'class' => 'email-field',
-                    'placeholder' => 'tomas@kamon.lt'
-                ]
-            ]
-        ]
-    ],
-    'buttons' => [
-        'save' => [
-            'title' => 'Submit the form',
-            'extra' => [
-                'attr' => [
-                    'class' => 'btn-submit',
-                ]
-            ]
-        ]
-    ]
+    'fields' =>
+        [
+            'name' => [
+                'label' => 'Name',
+                'type' => 'text',
+                // 'value' => 'name',
+                'extra' =>
+                    [
+                        'attr' =>
+                            [
+                                'class' => 'name-field',
+                                'placeholder' => 'Vardas',
+                            ],
+                    ],
+            ],
+            'email' => [
+                // 'label' => 'E-Mail',
+                'type' => 'email',
+                // 'value' => 'test-mail',
+                'extra' =>
+                    [
+                        'attr' =>
+                            [
+                                'class' => 'email-field',
+                                'placeholder' => 'lopo@email.com',
+                            ],
+                    ],
+            ],
+            'phone' => [
+                'label' => 'Phone',
+                'type' => 'tel',
+                // 'value' => 'phone',
+                'extra' =>
+                    [
+                        'attr' =>
+                            [
+                                'class' => 'phone-field',
+                                'placeholder' => 'Phone',
+                            ],
+                    ],
+            ],
+            'checkbox' => [
+                'label' => 'I have a bike',
+                'type' => 'checkbox',
+                'value' => 'Bike',
+                'extra' =>
+                    [
+                        'attr' =>
+                            [
+                                'class' => 'checkbox-field',
+                            ],
+                    ],
+            ],
+            'sex' => [
+                'label' => 'Lytis',
+                'type' => 'select',
+                'value' => 'female',
+                // 'id' => 'kazkoks',
+                'options' => [
+                    'male' => 'Kardanas',
+                    'female' => 'Mova',
+                    'other' => 'Kardamova',
+                ],
+            ],
+        ],
+    'buttons' =>
+        [
+            'save' =>
+                [
+                    'title' => 'Join',
+                    'extra' =>
+                        [
+                            'attr' =>
+                                [
+                                    'class' => 'big-button',
+                                ],
+                        ],
+                ],
+        ],
 ];
+
 ?>
-<!doctype html>
+
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>FORMS FORM ARRAYS</title>
+    <title>Form from array</title>
 </head>
+
 <body>
-<?php include 'templates/form.tpl.php'; ?>
+<?php include 'core/templates/form.tpl.php'; ?>
 </body>
+
 </html>
